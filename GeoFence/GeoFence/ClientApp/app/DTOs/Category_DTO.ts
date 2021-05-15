95780a759f36c3dd2b60aacdb01d660d37ce60f6
Arun Aravind <arunoyour@live.com>
2021-05-15 11:38:16 +0530

    teswt
    wtwe

--- GeoFence/GeoFence/ClientApp/app/DTOs/Category_DTO.ts
+++
@@ -1,9 +0,0 @@
-﻿export class CategoryDetailsDTO {
-    categoryId: number;
-    categoryName: string;
-    isDeleted: number;
-}
-export class CategoryDropDown {
-    categoryId: number;
-    categoryName: string;
-}
--- GeoFence/GeoFence/wwwroot/clientapp/out-tsc/app/DTOs/Item_DTO.js
+++
@@ -1,7 +0,0 @@
-var ItemDetailsDTO = /** @class */ (function () {
-    function ItemDetailsDTO() {
-    }
-    return ItemDetailsDTO;
-}());
-export { ItemDetailsDTO };
-//# sourceMappingURL=Item_DTO.js.map
